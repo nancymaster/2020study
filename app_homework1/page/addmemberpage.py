119bb1d89679e64c45880569994b4aa7b4b3aced
jia <xiao_nv_wu@126.com>
2020-08-02 23:05:30 +0800

    change dir

--- app_homework1/page/addmemberpage.py
+++ app_homework1/page/addmemberpage.py
@@ -1,3 +1,8 @@
-#123
-a=1
-print(a)
+from appium import webdriver
+desired_caps=dir()
+desired_caps['platformName']='Android'
+desired_caps['platformVersion']='6.0'
+desired_caps['deviceName']='emulator-5554'
+desired_caps['appPackage']='com.android.settings'
+desired_caps['appActivity']='com.android.settings.Settings'
+driver=webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
